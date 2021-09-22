34c46ae3edf8bdc6141cc4985185c36749027e36
Spartan-ash <ashishhsingh84@gmail.com>
2021-09-22 14:38:36 +0530

    declaration methods

---
+++ Basics-Variables-Values-Types/ShortDeclaration.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 50 //declaration of variable
+	fmt.Println(x)
+
+	x = 40 //assigning values
+	fmt.Println(x)
+
+	y := 100 + 30
+	fmt.Println(y)
+
+	z := "Ashish, Singh"
+	fmt.Println("Hello", z)
+}
