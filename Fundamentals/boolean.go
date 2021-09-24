26868844a57e4bb588e4eda3a656761aa04cb690
Spartan-ash <ashishhsingh84@gmail.com>
2021-09-24 15:14:30 +0530

    fundamentals

---
+++ Fundamentals/boolean.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var x bool
+
+func main() {
+	fmt.Println(x) //false since val is 0
+
+	a := 10
+	b := 20
+
+	fmt.Println(a == b) //check whehter val are equal or not and then prints the res
+}
