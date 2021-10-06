6f09eb815fb2203192ff59c9cbe834e5df44f897
Spartan-ash <ashishhsingh84@gmail.com>
2021-10-06 18:57:49 +0530

    for condition { } another variation

--- Exercise-Level-3/third.go
+++ Exercise-Level-3/third.go
@@ -6,9 +6,25 @@ package main
 
 import "fmt"
 
-func main() {
+/* func main() {
 	bd := 1997
 	for bd <= 2021 {
+		fmt.Println(bd)
+		bd++
+	}
+} */
+
+/*
+Create a for loop using this syntax
+for { }
+Have it print out the years you have been alive. */
+
+func main() {
+	bd := 1997
+	for {
+		if bd > 2021 {
+			break
+		}
 		fmt.Println(bd)
 		bd++
 	}
